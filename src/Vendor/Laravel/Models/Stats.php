2ba63e6942e6d8f4dcc9c6030c8db5dd4b4f1f47
DESKTOP-OOTVTF5\bramr <bramrobyns@hotmail.com>
2017-11-16 17:40:26 +0100

    do not calculate totals in statsForUser query

--- src/Vendor/Laravel/Models/Stats.php
+++ src/Vendor/Laravel/Models/Stats.php
@@ -1,16 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bramr
- * Date: 9/08/2016
- * Time: 18:35
- */
 
 namespace PragmaRX\Tracker\Vendor\Laravel\Models;
-
-
-use App\Image;
-use Carbon\Carbon;
 
 class Stats extends Base
 {
@@ -42,14 +32,15 @@ class Stats extends Base
         return $value / 100000;
     }
 
-    public function statsForUser($userId) {
-        $query = $this->newQuery()
+    public function statsForUser($userId)
+    {
+        return $this->newQuery()
             ->join('images', 'images.id', '=', 'stats.image_id')
-            ->join('users', function($join) use ($userId) {
+            ->join('users', function ($join) use ($userId) {
                 $join->on('users.id', '=', 'images.user_id')
                     ->where('user_id', $userId);
             })
-            ->rightjoin('calendar', function($join) {
+            ->rightjoin('calendar', function ($join) {
                 $join->on('stats.date', '=', 'calendar.date');
             })
             ->select($this->getConnection()->raw('calendar.date, tier_id as tier,
@@ -57,25 +48,7 @@ class Stats extends Base
             ->groupBy('calendar.date')
             ->groupBy('tier')
             ->where('calendar.date', '>=', Carbon::now()->startOfDay()->subDays(10))
-            ->where('calendar.date', '<=', Carbon::now()->endOfDay());
-
-        $result = $query->get();
-
-        $totalViewsA = $result->where('tier', 1)->sum('views');
-        $totalViewsB = $result->where('tier', 2)->sum('views');
-        $totalViewsC = $result->where('tier', 3)->sum('views');
-        $totalViewsD = $result->where('tier', 4)->sum('views');
-
-        $totalEarningsA = $result->where('tier', 1)->sum('earnings');
-        $totalEarningsB =  $result->where('tier', 2)->sum('earnings');
-        $totalEarningsC = $result->where('tier', 3)->sum('earnings');
-        $totalEarningsD = $result->where('tier', 4)->sum('earnings');
-        $totalEarnings = $result->sum('earnings');
-
-        $grouped = $result->groupBy('date');
-        return array(
-            'statsPerDay' => $grouped,
-            'totals' => [ 'views' => array($totalViewsA, $totalViewsB, $totalViewsC, $totalViewsD),
-                          'earnings' => array($totalEarningsA, $totalEarningsB, $totalEarningsC, $totalEarningsD, $totalEarnings)]);
+            ->where('calendar.date', '<=', Carbon::now()->endOfDay())
+            ->get();
     }
 }
