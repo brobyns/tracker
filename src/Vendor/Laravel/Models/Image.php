f9c858f8401178f5c5dec39e6e8c5e57f000d618
DESKTOP-OOTVTF5\bramr <bramrobyns@hotmail.com>
2018-03-23 18:38:39 +0100

    add user relation to image model

--- src/Vendor/Laravel/Models/Image.php
+++ src/Vendor/Laravel/Models/Image.php
@@ -5,4 +5,8 @@ namespace PragmaRX\Tracker\Vendor\Laravel\Models;
 class Image extends Base
 {
     protected $table = 'images';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
